7159333ba82148293a0ed3cc2be2fc450889b240
Shuxin Weng <shuxinweng@gmail.com>
2023-08-11 11:19:37 -0400

    Initial Commit

---
+++ ArtiPic/ArtiPicApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtiPicApp.swift
+//  ArtiPic
+//
+//  Created by Shuxin Weng on 8/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ArtiPicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArtiPic/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ArtiPic
+//
+//  Created by Shuxin Weng on 8/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
