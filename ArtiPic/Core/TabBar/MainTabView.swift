9cd39450b4244476c682d159b333a20e55e95924
dreamk0324 <dreamk0324@gmail.com>
2023-08-12 17:54:38 -0400

    user model and profile

---
+++ ArtiPic/Core/TabBar/MainTabView.swift
@@ -0,0 +1,41 @@
+//
+//  MainTabView.swift
+//  ArtiPic
+//
+//  Created by Dream K on 8/12/23.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            Text("Photo")
+                .tabItem {
+                    Image(systemName: "photo.stack")
+                }
+            
+            Text("AI")
+                .tabItem {
+                    Image(systemName: "paintbrush.pointed.fill")
+                }
+            
+            Text("Collection")
+                .tabItem {
+                    Image(systemName: "bookmark")
+                }
+            
+           Text("User")
+                .tabItem {
+                    Image(systemName: "person")
+                }
+        }
+        .accentColor(.black)
+    }
+}
+
+struct MainTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}
