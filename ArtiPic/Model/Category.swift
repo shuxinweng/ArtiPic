9452237a0029d3785783c1b965d72563edd6436c
Shuxin Weng <shuxinweng@gmail.com>
2023-08-16 00:41:14 -0400

    test merge

---
+++ ArtiPic/Model/Category.swift
@@ -0,0 +1,14 @@
+//
+//  Category.swift
+//  ArtiPic
+//
+//  Created by Shuxin Weng on 8/16/23.
+//
+
+import Foundation
+
+struct Category: Identifiable {
+    let id: String
+    let keyword: String
+    let photo_id: [String]
+}
