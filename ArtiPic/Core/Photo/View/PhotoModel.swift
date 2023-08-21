3de3d2b55119ba875487dbfba74ba30761a25f5a
Shuxin Weng <shuxinweng@gmail.com>
2023-08-21 01:55:49 -0400

    photos completely done

---
+++ ArtiPic/Core/Photo/View/PhotoModel.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoModel.swift
+//  ArtiPic
+//
+//  Created by Shuxin Weng on 8/21/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct PhotoInfo {
+    let image: UIImage
+    let keyword: String
+    let prompt: String
+}
