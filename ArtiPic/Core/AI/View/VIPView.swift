85319e34ae0a34585159bfcec54850fa6bcfce23
Shuxin Weng <shuxinweng@gmail.com>
2023-08-24 12:28:20 -0400

    isVIP works

--- ArtiPic/Core/AI/View/VIPView.swift
+++ ArtiPic/Core/AI/View/VIPView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import PassKit
 
 struct VIPView: View {
     @StateObject var paymentHandler = PaymentHandler()
@@ -20,41 +19,8 @@ struct VIPView: View {
     }
 }
 
-class PaymentHandler: NSObject, ObservableObject, PKPaymentAuthorizationViewControllerDelegate {
-    private var paymentRequest: PKPaymentRequest = {
-        let request = PKPaymentRequest()
-        request.merchantIdentifier = "merchant.com.shuxinweng.ArtiPic"
-        request.supportedNetworks = [.masterCard, .visa]
-        request.supportedCountries = ["US"]
-        request.merchantCapabilities = .capability3DS
-        request.countryCode = "US"
-        request.currencyCode = "USD"
-        request.paymentSummaryItems = [PKPaymentSummaryItem(label: "ArtiPic VIP", amount: NSDecimalNumber(decimalValue: 9.99))]
-        return request
-    }()
-
-    func presentPaymentAuthorizationViewController() {
-        let controller = PKPaymentAuthorizationViewController(paymentRequest: paymentRequest)
-        controller?.delegate = self
-        
-        if let windowScene = UIApplication.shared.connectedScenes.first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene {
-            if let rootViewController = windowScene.windows.first?.rootViewController {
-                rootViewController.present(controller!, animated: true, completion: nil)
-            }
-        }
-    }
-
-    func paymentAuthorizationViewControllerDidFinish(_ controller: PKPaymentAuthorizationViewController) {
-        controller.dismiss(animated: true, completion: nil)
-    }
-
-    func paymentAuthorizationViewController(_ controller: PKPaymentAuthorizationViewController, didAuthorizePayment payment: PKPayment, handler completion: @escaping (PKPaymentAuthorizationResult) -> Void) {
-        completion(PKPaymentAuthorizationResult(status: .success, errors: nil))
-    }
-}
-
-extension NSDecimalNumber {
-    convenience init(decimalValue: Decimal) {
-        self.init(decimal: decimalValue)
+struct VIPView_Previews: PreviewProvider {
+    static var previews: some View {
+        VIPView()
     }
 }
---
+++ ArtiPic/Core/AI/ViewModel/VIPViewModel.swift
@@ -0,0 +1,66 @@
+//
+//  VIPViewModel.swift
+//  ArtiPic
+//
+//  Created by Shuxin Weng on 8/24/23.
+//
+
+import Foundation
+import PassKit
+import FirebaseAuth
+import FirebaseFirestore
+
+class PaymentHandler: NSObject, ObservableObject, PKPaymentAuthorizationViewControllerDelegate {
+    private var paymentRequest: PKPaymentRequest = {
+        let request = PKPaymentRequest()
+        request.merchantIdentifier = "merchant.com.shuxinweng.ArtiPic"
+        request.supportedNetworks = [.masterCard, .visa]
+        request.supportedCountries = ["US"]
+        request.merchantCapabilities = .capability3DS
+        request.countryCode = "US"
+        request.currencyCode = "USD"
+        request.paymentSummaryItems = [PKPaymentSummaryItem(label: "ArtiPic VIP", amount: NSDecimalNumber(decimalValue: 9.99))]
+        return request
+    }()
+    
+    func presentPaymentAuthorizationViewController() {
+        let controller = PKPaymentAuthorizationViewController(paymentRequest: paymentRequest)
+        controller?.delegate = self
+        
+        if let windowScene = UIApplication.shared.connectedScenes.first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene {
+            if let rootViewController = windowScene.windows.first?.rootViewController {
+                rootViewController.present(controller!, animated: true, completion: nil)
+            }
+        }
+    }
+
+    func paymentAuthorizationViewControllerDidFinish(_ controller: PKPaymentAuthorizationViewController) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+
+    func paymentAuthorizationViewController(_ controller: PKPaymentAuthorizationViewController, didAuthorizePayment payment: PKPayment, handler completion: @escaping (PKPaymentAuthorizationResult) -> Void) {
+        
+        if let currentUser = Auth.auth().currentUser {
+            let currentUserID = currentUser.uid
+
+            // Update the VIP status in Firestore
+            let db = Firestore.firestore()
+            let userRef = db.collection("users").document(currentUserID)
+            userRef.updateData(["isVIP": true]) { error in
+                if let error = error {
+                    print("Error updating VIP status: \(error)")
+                } else {
+                    print("VIP status updated successfully")
+                }
+            }
+        }
+        
+        completion(PKPaymentAuthorizationResult(status: .success, errors: nil))
+    }
+}
+
+extension NSDecimalNumber {
+    convenience init(decimalValue: Decimal) {
+        self.init(decimal: decimalValue)
+    }
+}
